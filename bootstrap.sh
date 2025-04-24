4498f45672929d033188951fd3418e7494104565
Fabrizio Anichini <34816005+fabrizioanichini@users.noreply.github.com>
2025-04-24 13:53:28 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,21 +3,39 @@ set -e
 
 cd ~/dotfiles/stow
 
+info() {
+  echo -e "\n🔧 $1"
+}
+
+remove_if_exists() {
+  local file="$1"
+  if [ -f "$HOME/$file" ] && [ ! -L "$HOME/$file" ]; then
+    info "Removing existing $file (not a symlink)..."
+    rm "$HOME/$file"
+  fi
+}
+
+remove_if_exists ".bashrc"
+remove_if_exists ".bash_profile"
+
+info "Stowing common..."
 stow -t ~ common
 
 OS="$(uname -s)"
 case "$OS" in
   Darwin)
-    echo "Stowing macOS-specific dotfiles..."
-    stow -t ~ --adopt macos
+    info "Stowing macOS-specific dotfiles..."
+    stow -t ~ macos
     ;;
   Linux)
     if grep -q Microsoft /proc/version; then
-      echo "Stowing WSL-specific dotfiles..."
-      stow -t ~ --adopt wsl
+      info "Stowing WSL-specific dotfiles..."
+      stow -t ~ wsl
     else
-      echo "Stowing Arch-specific dotfiles..."
-      stow -t ~ --adopt arch
+      info "Stowing Arch-specific dotfiles..."
+      stow -t ~ arch
     fi
     ;;
 esac
+
+info "✅ Dotfiles setup complete."
